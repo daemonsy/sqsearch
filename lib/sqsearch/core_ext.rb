a415caa9e2c12a6b96b22d1e68110bd0bceabfbd
Damon Aw <daemonsy@gmail.com>
2012-07-23 15:00:13 +0800

    Change the where clause to prevent using direct string.

--- lib/sqsearch/core_ext.rb
+++ lib/sqsearch/core_ext.rb
@@ -9,9 +9,9 @@ module Sqsearch
     def search(query,options={})
       search_obj = Search.new(@fields)
       @fields.each do |field|
-        search_obj.query_string << "#{field} like '%#{query}%'"
+        search_obj.query_string << "#{field} like :query"
       end
-      search_obj.results = where(search_obj.query_string.join(" OR "))
+      search_obj.results = where(search_obj.query_string.join(" OR "), {:query=>"%#{query}%"})
       search_obj
     end
   end
